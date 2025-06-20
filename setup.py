d15cf3b2b2826a524f2b45c00c91bb35140605d4
choimagon <48877378+choimagon@users.noreply.github.com>
2025-06-21 01:39:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='colorlog',                 # 패키지 이름
     version='0.1',                   # 버전
-    py_modules=['colorlog'], 
+    packages=['colorlog'], 
 )
